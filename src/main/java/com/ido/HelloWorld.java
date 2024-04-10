ebc400c0058221f5ba8ec4bdc6bd1f7e281905fc
root <nikg1999@gmail.com>
2024-04-10 14:10:17 +0300

    add new branch features, add my name to hello world massage

--- src/main/java/com/ido/HelloWorld.java
+++ src/main/java/com/ido/HelloWorld.java
@@ -6,6 +6,7 @@ package com.ido;
  
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!\n Nick Gofman");
     }
 }
+
