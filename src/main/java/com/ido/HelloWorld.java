e933bbfc2c40a98f3530c916312290a5a9905209
root <nikg1999@gmail.com>
2024-04-11 15:01:35 +0300

    intentionally break the java code

--- src/main/java/com/ido/HelloWorld.java
+++ src/main/java/com/ido/HelloWorld.java
@@ -4,7 +4,14 @@ package com.ido;
  * Hello world!
  */
  
-public class HelloWorld {
+// public class HelloWorld {
+    // public static void main(String[] args) {
+        // System.out.println("Hello World!\n Nick Gofman");
+    // }
+// }
+
+
+ublic class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!\n Nick Gofman");
     }
