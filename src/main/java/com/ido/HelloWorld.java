aa31594b92436842634186dff1b40eee2396a27c
root <nikg1999@gmail.com>
2024-04-11 15:23:54 +0300

    remove intentionally java error

--- src/main/java/com/ido/HelloWorld.java
+++ src/main/java/com/ido/HelloWorld.java
@@ -4,16 +4,12 @@ package com.ido;
  * Hello world!
  */
  
-// public class HelloWorld {
-    // public static void main(String[] args) {
-        // System.out.println("Hello World!\n Nick Gofman");
-    // }
-// }
-
-
-ublic class HelloWorld {
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!\n Nick Gofman");
     }
 }
+
+
+
 
